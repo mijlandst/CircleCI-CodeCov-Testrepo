885cd6e57954184130178f7b7c05917cd7f3e3c2
Stijn Mijland <s.t.mijland@student.rug.nl>
2017-08-29 15:43:59 +0200

    made main abstract to fix coverage maybe?

--- code/src/main/java/test/Main.java
+++ code/src/main/java/test/Main.java
@@ -4,7 +4,7 @@ package test;
  * A dummy program to enable the creation of elementary test to verify that
  * the CI works.
  */
-abstract public final class Main {
+abstract public class Main {
 
     /**
      * Prints "Hello, world!" to standard output.
